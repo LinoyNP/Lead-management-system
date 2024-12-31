2b393089c562d73aaf1011b7d862818f5852370b
YaelSimanTov <129066536+YaelSimanTov@users.noreply.github.com>
2024-12-31 22:18:23 +0200

    Add express server wuth basic endpoints

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;  
+
+app.get('/', (req, res) => {
+    res.send('Hello ,Word!');
+})
+
+app.get('/home', (req, res) => {
+    res.send('Hello ,from home!');
+})
+
+app.get('/hi_byJSON', (req, res) => {
+    res.send('{ "text": "Hello ,from home!" }');
+})
+
+
+
+app.listen(PORT, () => {
+    console.log("listening on port", PORT);
+})
