f2de0e1b94d0a2c3840c21af674bdb4325f5c791
LinoyNP <129064940+LinoyNP@users.noreply.github.com>
2025-01-04 18:37:09 +0200

    Update the "Search By" panel.
    The panel allows selecting a category by which the sales agent will search for the lead (search by name, product, email, location, or company).

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+/*
+-----Searching--------
+*/
 function filteringSearchBy(){
     // Open the "Filter by" menu
+    const button = document.getElementById("openSearchingBY");
     const panel = document.getElementById("panel-SearchingBY");
+    const buttonRect = button.getBoundingClientRect();
+    panel.style.left = `${buttonRect.left + window.scrollX}px`;
     
     if (panel.classList.contains("open")) {
         panel.classList.remove("open");
@@ -10,14 +16,90 @@ function filteringSearchBy(){
     }
 }
 
-function Sort(){
-     // Open the "sorting" menu
-     const panel = document.getElementById("panel-sorting");
+
+function ListenersForSearchAndFiltering(){
+    // this function changes the description in the search engine according to the "Search by" selection.
+    const searchInput = document.getElementById("Search");
+    const radioButtonsSearchBY = document.querySelectorAll('input[name="radio-buttons-search"]');
+    const searchButton = document.getElementById("submitSearching");
+    const panel = document.getElementById("panel-SearchingBY");
+
+    let selectedSearchBy = "name"; //The default search is by name.
     
-     if (panel.classList.contains("open")) {
-         panel.classList.remove("open");
-     } 
-     else {
-         panel.classList.add("open");
-     }
+    radioButtonsSearchBY.forEach((radio) => {
+        radio.addEventListener("change", (event) => {
+            selectedSearchBy = event.target.value;
+            switch (selectedSearchBy) {
+                case "Product":
+                    searchInput.placeholder = "Search by Product";
+                    break;
+                case "Email":
+                    searchInput.placeholder = "Search by Email";
+                    break;
+                case "Location":
+                    searchInput.placeholder = "Search by Location";
+                    break;
+                case "Company":
+                    searchInput.placeholder = "Search by Company";
+                    break;
+                default:
+                    searchInput.placeholder = "Search by Name";
+            }
+            setTimeout(() => {
+            panel.classList.remove("open");
+            }, 250);
+        });
+    });
+
+    // searchButton.addEventListener("click", () => {
+    //     const searchValue = searchInput.value; // The value typed in the search field
+    
+    //     if (!searchValue) {
+    //         alert("Please enter a search value.");
+    //         return;
+    //     }
+    //     console.log(searchValue, selectedSearchBy);
+    //     SearchingFromDB(searchValue, selectedSearchBy);
+    // });
+}
+
+function inputFromEngineSearch(){
+    const searchInput = document.getElementById("Search");
+    const searchValue = searchInput.value; // The value typed in the search field
+    const radioButtonsSearchBY = document.querySelectorAll('input[name="radio-buttons-search"]');
+
+        if (!searchValue) {
+            alert("Please enter a search value.");
+            return;
+        }
+        
+        radioButtonsSearchBY.forEach((radio) => {
+            radio.addEventListener("change", (event) => {
+                selectedSearchBy = event.target.value;
+                SearchingFromDB(searchValue);
+        });
+    });
+}
+
+function SearchingFromDB(content, searchKind){
+    prompt(`your search is: ${content} by kind: ${searchKind}`);
+}
+
+/*
+-----Sorting--------
+*/
+function Sort(){
+    // Open the "sorting" menu
+    const panel = document.getElementById("panel-sorting");
+    const sortButton = document.getElementById("openSorting");
+   
+    const buttonRect = sortButton.getBoundingClientRect();
+    panel.style.left = `${buttonRect.left + window.scrollX}px`;
+
+    if (panel.classList.contains("open")) {
+        panel.classList.remove("open");
+    } 
+    else {
+        panel.classList.add("open");
+    }
 }
